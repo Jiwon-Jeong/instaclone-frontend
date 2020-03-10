dec1ecd616a38b66eba57ba45a7d3a91395294a4
JIWON <ziwon16@gmail.com>
2020-03-10 18:18:48 +0900

    Feed: Post Component Fin.

--- src/Styles/GlobalStyles.js
+++ src/Styles/GlobalStyles.js
@@ -7,6 +7,7 @@ export default createGlobalStyle`
 
     * {
         box-sizing: border-box;
+        font-family:-apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
     }
     body{
         background-color: ${props => props.theme.bgColor};
@@ -19,6 +20,7 @@ export default createGlobalStyle`
         color: ${props => props.theme.blueColor};
         text-decoration: none;
     }
+
     input:focus{
         outline:none;
     }
