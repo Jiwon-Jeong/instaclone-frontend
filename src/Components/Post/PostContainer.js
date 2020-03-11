8329363956b1a26b060a6cfd8f6cf0a9007ddc2f
JIWON <ziwon16@gmail.com>
2020-03-11 17:58:13 +0900

    Feed: createComment Fin.

--- src/Components/Post/PostContainer.js
+++ src/Components/Post/PostContainer.js
@@ -4,6 +4,7 @@ import useInput from "../../Hooks/useInput";
 import PostPresenter from "./PostPresenter";
 import { useMutation } from "react-apollo-hooks";
 import { TOGGLE_LIKE, ADD_COMMENT } from "./PostQueries";
+import { toast } from "react-toastify";
 
 const PostContainer = ({
   id,
@@ -19,6 +20,7 @@ const PostContainer = ({
   const [isLikedS, setIsLiked] = useState(isLiked);
   const [likeCountS, setLikeCount] = useState(likeCount);
   const [currentItem, setCurrentItem] = useState(0);
+  const [selfComments, setSelfComments] = useState([]);
   const comment = useInput("");
   const [toggleLikeMutation] = useMutation(TOGGLE_LIKE, {
     variables: { postId: id }
@@ -46,6 +48,24 @@ const PostContainer = ({
     }
   };
 
+  const onKeyPress = async event => {
+    const { which } = event;
+    if (which === 13) {
+      event.preventDefault();
+      try {
+        const {
+          data: { addComment }
+        } = await addCommentMutation();
+        setSelfComments([...selfComments, addComment]);
+        comment.setValue("");
+      } catch {
+        toast.error("Cant send comment");
+      }
+    }
+  };
+
+  let createdAtS = useState(createdAt)[0].slice(0, 10);
+
   return (
     <PostPresenter
       user={user}
@@ -55,12 +75,14 @@ const PostContainer = ({
       caption={caption}
       isLiked={isLikedS}
       comments={comments}
-      createdAt={createdAt}
+      createdAt={createdAtS}
       newComment={comment}
       setIsLiked={setIsLiked}
       setLikeCount={setLikeCount}
       currentItem={currentItem}
       toggleLike={toggleLike}
+      onKeyPress={onKeyPress}
+      selfComments={selfComments}
     />
   );
 };
