b741beb8e372e92a2583261d3242bda2aa40539e
JIWON <ziwon16@gmail.com>
2020-04-08 01:54:46 +0900

    Change to Real URL

--- src/Apollo/Client.js
+++ src/Apollo/Client.js
@@ -2,20 +2,15 @@ import ApolloClient from "apollo-boost";
 import { defaults, resolvers } from "./LocalState";
 
 export default new ApolloClient({
-  uri: "http://localhost:4000",
+  uri:
+    process.env.NODE_ENV === "development"
+      ? "http://localhost:4000"
+      : "https://jnstagram-backend.herokuapp.com/",
   clientState: {
     defaults,
-    resolvers
+    resolvers,
   },
   headers: {
-    Authorization: `Bearer ${localStorage.getItem("token")}`
-  }
-  // request: async operation => {
-  //   const token = await localStorage.getItem("token");
-  //   operation.setContext({
-  //     headers: {
-  //       Authorization: token ? `Bearer ${token}` : ""
-  //     }
-  //   });
-  // }
+    Authorization: `Bearer ${localStorage.getItem("token")}`,
+  },
 });
